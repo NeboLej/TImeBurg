127acbdf46f18e95f892b4324be3b97afc6435be
Nebo <kindersmail08@gmail.com>
2023-01-09 19:55:37 +0300

    init commit

---
+++ TImeBurg/TImeBurgApp.swift
@@ -0,0 +1,17 @@
+//
+//  TImeBurgApp.swift
+//  TImeBurg
+//
+//  Created by Nebo on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TImeBurgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
